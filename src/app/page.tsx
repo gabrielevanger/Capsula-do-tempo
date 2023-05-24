254ca969a007e259c22a64dc3b42a7fbe76f5a17
unknown <gabriel.evanger@gmail.com>
2023-05-24 14:54:03 -0300

    Estilizando em Tailwind CSS
    Adicionando estilização nas page
    
    Signed-off-by: unknown <gabriel.evanger@gmail.com>

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,7 @@
 import { User } from 'lucide-react'
+import Image from 'next/image'
+
+import nlwLogo from '../assets/nlw-spacetime-logo.svg'
 
 export default function Home() {
   return (
@@ -9,12 +12,38 @@ export default function Home() {
         <div className="absolute right-0 top-1/2 h-[288px] w-[526px] -translate-y-1/2 translate-x-1/2 rounded-full bg-purple-700 opacity-50 blur-full" />
 
         {/* Stripes */}
-        <div className="absolute bottom-0 right-2 top-0 w-2 bg-stripes pr-2" />
-        <a href="" className="flex items-center gap-3 text-left">
-          <div className="flex h-10 w-10 items-center justify-center rounded-full bg-gray-400 ">
+        <div className="absolute bottom-0 right-2 top-0 w-2 bg-stripes" />
+        {/* Sign In */}
+        <a
+          href=""
+          className="flex items-center gap-3 text-left transition-colors hover:text-gray-50"
+        >
+          <div className="flex h-10 w-10 items-center justify-center rounded-full bg-gray-400">
             <User className="h-5 w-5 text-gray-500" />
           </div>
+          <p className="max-w-[140px] text-sm leading-snug">
+            <span className="underline">Cria sua conta</span> e salve suas
+            memórias!
+          </p>
         </a>
+        {/* Hero */}
+        <div className="space-y-5">
+          <Image src={nlwLogo} alt="NLW Spacetime" />
+
+          <div className="max-w-[420px] space-y-1">
+            <h1 className="mt-5 text-5xl font-bold leading-tight text-gray-50">
+              Sua cápsula do tempo
+            </h1>
+            <p className="mt-' text-lg leading-relaxed">
+              Colecione momentos marcantes de suas jornada e compartilhe (se
+              quiser) com o mundo!
+            </p>
+          </div>
+
+          <a className="inline-block" href="">
+            CADASTRAR LEMBRANÇA
+          </a>
+        </div>
       </div>
 
       {/* Right */}
